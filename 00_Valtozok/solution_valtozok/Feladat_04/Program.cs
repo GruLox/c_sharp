fc3a616f4bbd1fa866eb0c2dbf53a9016fb38a23
Marcell Grund <grundmarcell@gmail.com>
2023-09-09 15:37:52 +0200

    valtozok_feladat_04

---
+++ 00_Valtozok/solution_valtozok/Feladat_04/Program.cs
@@ -0,0 +1,7 @@
+﻿string manufacturer = "Sukhoj";
+string model = "SU-35S";
+int releaseYear = 2008;
+double maxVelocity = 2.4;
+string metricVelocityUnit = "m/s";
+
+Console.WriteLine($"Oroszország legmodernebb vadászrepülőgépe a {releaseYear}-ban bemutatott {manufacturer} gyártotta {model} amely képes a {maxVelocity} Mach sebességre, ami 720{metricVelocityUnit}-nak felel meg.");
