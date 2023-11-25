f55b8d81778c0a5448913bca61849219a42011b5
GruLox <grundmarcell@gmail.com>
2023-11-25 12:18:51 +0100

    functions_feladat_11

---
+++ 11_fuggvenyek/solution_functions/Feladat_11/Program.cs
@@ -0,0 +1,21 @@
+﻿using Feladat_11;
+using IOLibrary;
+
+List<Worker> workers = GetWorkers(5);
+
+foreach (Worker worker in workers)
+{
+    Console.WriteLine(worker);
+}
+
+List<Worker> GetWorkers(int count)
+{
+    List<Worker> workers = new List<Worker>();
+    for (int i = 0; i < count; i++)
+    {
+        string name = ExtendedConsole.ReadString($"Kérem adja meg a {i}. dolgozó nevét: ");
+        int hoursWorked = ExtendedConsole.ReadInteger($"Kérem adja meg a {i}. dolgozó ledolgozott óráinak számát: ", int.MaxValue, 0);
+        workers.Add(new Worker(name, hoursWorked));
+    }
+    return workers;
+}
---
+++ 11_fuggvenyek/solution_functions/Feladat_11/Worker.cs
@@ -0,0 +1,29 @@
+﻿namespace Feladat_11;
+
+public class Worker
+{
+    const int BASE_WAGE = 1000;
+    const int OVERTIME_WAGE = 1500;
+
+    public string Name { get; set; }
+    public int HoursWorked { get; set; }
+    public double Salary => CalculateSalary();
+    
+
+    public Worker(string name, int hoursWorked)
+    {
+        Name = name;
+        HoursWorked = hoursWorked;
+    }
+
+    private double CalculateSalary()
+    {
+        return HoursWorked <= 40 ? HoursWorked * BASE_WAGE : 40 * BASE_WAGE + (HoursWorked - 40) * OVERTIME_WAGE;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {HoursWorked} órát dolgozott és {Salary} Ft-ot keresett.";
+    }
+}
+
