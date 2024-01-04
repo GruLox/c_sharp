1122ed9d3431c77514d1739783f3454833f3c511
Grund Marcell <grundmarcell@gmail.com>
2024-01-04 09:31:42 +0100

    arrays_car

---
+++ 06_Egydimenzios_tombok/Egydimenzios_tombok/Feladat_08/Car.cs
@@ -0,0 +1,45 @@
+﻿namespace Feladat_07;
+
+public class Car
+{
+    const int MAX_VELOCITY = 200;
+    const int MIN_VELOCITY = 30;
+
+    public string LicensePlate { get; set; }
+
+    public int Velocity { get; set; }
+
+    public int SpeedingFine => GetSpeedingFine(Velocity);
+
+
+
+    public Car() { }
+
+    public Car(string licensePlate, int velocity)
+    {
+        LicensePlate = licensePlate;
+        Velocity = velocity;
+    }
+
+    public override string ToString()
+    {
+        return $"{LicensePlate} rendszámú autó {Velocity} km/h, büntetése: {SpeedingFine}";
+    }
+
+    private static int GetRandomVelocity()
+    {
+        Random rnd = new Random();
+        return rnd.Next(MIN_VELOCITY, MAX_VELOCITY);
+    }
+
+    private static int GetSpeedingFine(int velocity)
+    {
+        return velocity switch
+        {
+            <= 90 => 0,
+            <= 100 => 10000,
+            <= 110 => 20000,
+            _ => 30000
+        };
+    }
+}
---
+++ 06_Egydimenzios_tombok/Egydimenzios_tombok/Feladat_08/Program.cs
@@ -0,0 +1,51 @@
+﻿using Custom.Library.ConsoleExtensions;
+using Feladat_07;
+
+const int CAR_COUNT = 2;
+
+Car[] cars = GetCars(CAR_COUNT);
+
+Car[] speedingCars = cars.Where(car => car.SpeedingFine > 0).ToArray();
+Console.WriteLine("Gyorshajtók:");
+foreach (var car in speedingCars)
+{
+    Console.WriteLine(car);
+
+}
+
+int sumOfFines = speedingCars.Sum(car => car.SpeedingFine);
+Console.WriteLine($"Összes büntetés: {sumOfFines} Ft");
+
+
+Car fastestCar = GetFastestCar(speedingCars);
+Console.WriteLine($"A leggyorsabb autó: {fastestCar.LicensePlate} - {fastestCar.Velocity}");
+
+int lawfulCommuterCount = CAR_COUNT - speedingCars.Length;
+double lawfulCommuterRatio = ((double)lawfulCommuterCount / CAR_COUNT) * 100;
+Console.WriteLine($"{lawfulCommuterCount} autó közlekedett szabályosan, ez az összes autó {lawfulCommuterRatio} százaléka");
+
+bool wasVelocityOf60 = cars.Any(car => car.Velocity == 60);
+Console.WriteLine($"{(wasVelocityOf60 ? "volt" : "nem volt")} 60 km/h-val közlekedő autó.");
+
+
+static Car[] GetCars(int count)
+{
+    Car[] cars = new Car[count];
+    for (int i = 0; i < count; i++)
+    {
+        string licensePlate = ExtendedConsole.ReadString($"Adja meg a {i + 1}. autó rendszámát: ");
+        int velocity = ExtendedConsole.ReadInteger($"Adja meg a {i + 1}. autó sebességét: ", int.MaxValue, 0);
+
+        cars[i] = new Car(licensePlate, velocity);
+    }
+    return cars;
+}
+
+static Car GetFastestCar(Car[] cars)
+{
+    int highestVelocity = cars.Max(car => car.Velocity);
+
+    Car fastestCar = cars.First(car => car.Velocity == highestVelocity);
+
+    return fastestCar;
+}
